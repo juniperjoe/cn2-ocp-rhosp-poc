d7d3c10a1a3a433555de5a61bf42a91a5243c0ce
juniperjoe <josephw@juniper.net>
2023-05-19 14:18:11 -0700

    Remove cruft

--- apply2.sh
+++ apply2.sh
@@ -4,5 +4,6 @@ kubectl apply -f baremetalset.yaml
 kubectl cp rhsm-controller.yaml -nopenstack -c openstackclient openstackclient:/home/cloud-admin
 kubectl cp rhsm-compute.yaml -nopenstack -c openstackclient openstackclient:/home/cloud-admin
 kubectl exec -it -nopenstack openstackclient -c openstackclient -- ansible-playbook -i /home/cloud-admin/ctlplane-ansible-inventory /home/cloud-admin/rhsm-controller.yaml
+sleep 60
 kubectl exec -it -nopenstack openstackclient -c openstackclient -- ansible-playbook -i /home/cloud-admin/ctlplane-ansible-inventory /home/cloud-admin/rhsm-compute.yaml
 
